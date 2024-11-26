467053a9d2e3754cc1d0476759c3191295d2d373
WHSVic <victorp@me.com>
2024-11-26 12:17:16 -0500

    Initial code commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//init
+const express = require("express");
+const cors = require("cors");
+const app = express();
+//middleware
+app.use(cors());
+//routes
+app.get("/api/message", (req, res) => {
+	res.json({message:"Hi, my name is Victor"});
+});
+//start server
+const PORT = 3000;
+app.listen(PORT,() => {
+	console.log("Backend server is running on" + PORT);
+});
